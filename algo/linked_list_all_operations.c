2d0a196660492d45a6f15037ba7ab7a3b1a592b9
Bogdan Condurache <bcondurache@gmail.com>
2018-07-02 00:56:04 +0300

    Intalnire 2 SDA 01.07.2018

---
+++ algo/linked_list_all_operations.c
@@ -0,0 +1,79 @@
+#include <stdio.h>
+#include <stdlib.h>
+ 
+typedef struct Node {
+	int data;
+	struct Node *next;
+} Node;
+
+void deleteNode(Node **head_ref, int key) {
+	Node* temp = *head_ref, *prev;
+	if (temp != NULL && temp->data == key) {
+		*head_ref = temp->next;
+		free(temp);
+		return;
+	}
+
+	while (temp != NULL && temp->data != key) {
+		prev = temp;
+		temp = temp->next;
+	}
+ 
+	if (temp == NULL)
+		return;
+	prev->next = temp->next;
+	free(temp);
+}
+ 
+void push(Node** head_ref, int new_data) {
+		Node* new_node = (Node*)malloc(sizeof(Node));
+		new_node->data = new_data;
+		new_node->next = (*head_ref);
+		*head_ref = new_node;
+}
+
+void insertAfter(Node* prev_node, int new_data) {
+		if (prev_node == NULL)
+			return;
+
+		Node* new_node = (Node*)malloc(sizeof(Node));
+		new_node->data = new_data;
+		new_node->next = prev_node->next;
+		prev_node->next = new_node;
+}
+ 
+void append(Node** head_ref, int new_data) {
+		Node* new_node = (Node*)malloc(sizeof(Node));
+		Node *last = *head_ref;
+		new_node->data = new_data;
+		new_node->next = NULL;
+		if (*head_ref == NULL) {
+			*head_ref = new_node;
+			return;
+		}
+		while (last->next != NULL)
+			last = last->next;
+		last->next = new_node;
+}
+ 
+void printList(Node *node) {
+	while (node != NULL) {
+		 printf(" %d ", node->data);
+		 node = node->next;
+	}
+}
+ 
+int main() {
+	Node* head = NULL;
+	append(&head, 6);
+	push(&head, 7);
+	push(&head, 1);
+	append(&head, 4);
+	insertAfter(head->next, 8);
+	printf("\n Lista creata: ");
+	printList(head);
+	deleteNode(&head, 1);
+	printf("\n Lista dupa stergere: ");
+	printList(head);
+	return 0;
+}
--- algo/problema_examen_1.c
+++ algo/problema_examen_1.c
@@ -1,5 +1,5 @@
 /* Se citesc doua multimi de numere si se face cate o lista simplu inlantuita pentru fiecare
-si o lista simplu inlantuita pentru reuniunea multimiilor
+si o lista simplu inlantuita pentru reuniunea multimilor
 */
 
 #include <stdio.h>
@@ -11,10 +11,9 @@ typedef struct Node {
 } Node;
 
 void printList(struct Node *n) {
-	while (n != NULL)
-	{
-		 printf(" %d ", n->data);
-		 n = n->next;
+	while (n != NULL) {
+		printf(" %d ", n->data);
+		n = n->next;
 	}
 }
 
@@ -22,27 +21,74 @@ void add(Node** head, int data) {
 	Node *temp = (Node*)malloc(sizeof(Node));
 	temp->data = data;
 	temp->next = NULL;
-	if (*head == NULL) {
+	if (*head == NULL)
 		*head = temp;
-	}
 	else {
 		(*head)->next = temp;
 		(*head) = (*head)->next;
 	}
 }
 
+/* Returneaza nodul din lista de pe pozitia data */
+Node* get(Node* list, int position) {
+	while (position--)
+		list = list->next;
+	return list;
+}
+
+/* returneaza 1 daca `value` e in lista, 0 daca nu */
+short exist(Node* list, int value) {
+	while (list != NULL) {
+		if (list->data == value)
+			return 1;
+		list = list->next;
+	}
+	return 0;
+}
+
 int main(int argc, char *argv[]) {
-	Node *head, *first;
+	int m, n, i, val;
+	Node *head, *head2, *head3;
+	Node *list_one, *list_two, *list_three;
 	head = NULL;
-	first = (Node*)malloc(sizeof(Node));
-	int n, i, val;
+	head2 = NULL;
+	head3 = NULL;
+	list_one = (Node*)malloc(sizeof(Node));
+	list_two = (Node*)malloc(sizeof(Node));
+	list_three = (Node*)malloc(sizeof(Node));
+	scanf("%d", &m);
 	scanf("%d", &n);
-	for (i = 0; i < n; i++) {
+	for (i = 0; i < m; i++) {
 		scanf("%d", &val);
 		add(&head, val);
 		if (i == 0) {
-			first = head;
+			list_one = head;
 		}
 	}
-	printList(first);
+	for (i = 0; i < n; i++) {
+		scanf("%d", &val);
+		add(&head2, val);
+		if (i == 0) {
+			list_two = head2;
+		}
+	}
+	for (i = 0; i < m; i++) {
+		Node* temp = get(list_one, i);
+		add(&head3, temp->data);
+		if (i == 0) {
+			list_three = head3;
+		}
+	}
+	for (i = 0; i < n; i++) {
+		Node* temp = get(list_two, i);
+		if (!exist(list_one, temp->data))
+			add(&head3, temp->data);
+	}
+
+	printf("\n Lista 1: ");
+	printList(list_one);
+	printf("\n Lista 2: ");
+	printList(list_two);
+	printf("\n Lista 3: ");
+	printList(list_three);
 }
